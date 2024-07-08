ad5bf9e5cce4dbd22a1d27986b1a95994ab614ee
MT549 <monica.turaka@eagles.oc.edu>
2024-07-07 22:36:08 -0500

    Generating Resume in Template1 format

--- src/components/ResumeBuilder.vue
+++ src/components/ResumeBuilder.vue
@@ -52,9 +52,9 @@
             <v-row>
               <v-col cols="6">
                 <v-text-field
-                  v-model="linkedinURL"
+                  v-model="location"
                   class="mb-2"
-                  label="LinkedIn URL"
+                  label="Location"
                   hide-details
                 ></v-text-field>
               </v-col>
@@ -71,7 +71,7 @@
           </v-card>
         </template>
 
-<template v-slot:item.2>
+        <template v-slot:item.2>
           <v-card title="Professional Summary" >
             <br/>
             <v-row class="ml-3">
@@ -241,7 +241,6 @@
           </v-card>
         </template>
 
-
         <template v-slot:item.4>
           <v-card title="Experience">
             <br/>
@@ -305,7 +304,7 @@
                       @click="this.notesOverlay = !this.notesOverlay"
                       block
                     >
-                      Responsibilities
+                      Notes
                     </v-btn>
                   </v-col>
                   <v-col cols="4">
@@ -359,7 +358,6 @@
           </v-card>
         </template>
       
-
         <template v-slot:item.5>
           <v-card title="Skills" flat>
             <br/>
@@ -428,8 +426,7 @@
           </v-card>
         </template>
 
-
-        <template v-slot:item.5>
+        <template v-slot:item.6>
           <v-card flat>
             <v-row>
               <v-col cols="9">
@@ -446,6 +443,8 @@
                   size="large"
                   variant="elevated"
                   rounded="xl"
+                  @click="preview"
+                  block
                 >
                   Preview
                 </v-btn>
@@ -456,6 +455,7 @@
                   size="large"
                   variant="elevated"
                   rounded="xl"
+                  block
                 >
                   Save
                 </v-btn>
@@ -466,33 +466,276 @@
                   size="large"
                   variant="elevated"
                   rounded="xl"
+                  block
+                  @click="exportToPDF"
                 >
                   Download
                 </v-btn>
               </v-col>
             </v-row>
           </v-card>
-          <v-card title="Preview" flat>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+          <v-card flat v-if="tempalte == 'Template 1' && showTemplate" width="70%" color="#fff">
+            
+                  <section class="pdf-item">
+                    <v-card class="mx-auto px-10 py-8 overflow-y-auto" color="#fff">
+                      <div style="text-align:center;">
+                        <h2>{{ resumeDetails.fullName }}</h2>
+                        <p>{{ resumeDetails.location }} | {{ resumeDetails.phoneNumber }} | {{ resumeDetails.email }} | {{ resumeDetails.email }}</p>
+                        <hr/>
+                      </div>
+
+                      <div>
+                        <br/>
+                        <h3>Professional Summary</h3>
+                        <hr/>
+                        <p>
+                          {{ resumeDetails.professionalSummary }}
+                        </p>
+                      </div>
+
+                      <div>
+                        <br/>
+                        <h3>Education</h3>
+                        <hr/>
+                        <div v-for="edu in resumeDetails.educationDetails">
+                          <br/>
+                          <v-row class="px-3">
+                            <p style="font-size:16px;">
+                              <b>{{ edu.institution }}, {{ edu.location }} </b>
+                            </p>
+                            <v-spacer></v-spacer>
+                            <p style="font-size:14px;">
+                              {{ edu.eduStartDate }} - {{ edu.eduEndDate }}
+                            </p>
+                          </v-row>
+                          <br style="line-height:0px;margin: 6px 0;display: block;"/>
+                          <div style="font-size:14px;">
+                            <p>{{ edu.degree }}</p>
+                            <p>GPA: {{ edu.gpa }}</p>
+                            <p>Awards: {{ edu.awards.toString() }}</p>
+                            <p>Courses: {{ edu.courses.toString() }}</p>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div>
+                        <br/>
+                        <h3>Experience</h3>
+                        <hr/>
+                        <div v-for="experience in resumeDetails.experienceDetails">
+                          <br/>
+                          <v-row class="px-3">
+                            <p  style="font-size:16px;">
+                              <b>{{ experience.employer }}, {{ experience.jobTitle }}, {{ experience.jobLocation }} </b>
+                            </p>
+                            <v-spacer></v-spacer>
+                            <p>
+                              Date ({{ experience.jobStartDate }} - {{ experience.jobEndDate }})
+                            </p>
+                          </v-row>
+                          <br style="line-height:0px;margin: 6px 0;display: block;"/>
+                          <div style="font-size:14px;">
+                            <p v-for="note in experience.experienceNote">●	{{ note }}</p>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div>
+                        <br/>
+                        <h3>Skills</h3>
+                        <hr/>
+                        <p v-for="skill in resumeDetails.skills">●	{{ skill.skillName }} : {{ skill.skillLevel }}</p>
+
+                      </div>
+                    </v-card>
+            </section>
+          </vue-html2pdf>
           </v-card>
         </template>
       </v-stepper>
     </v-container>
     <br/>
+
+    <v-overlay
+      :model-value="awardsOverlay"
+      class="align-center justify-center"
+      persistent
+    >
+      <v-card class="mx-auto px-10 py-3" width="850px" height="80px" color="#711429">
+        <v-row >
+          <v-col cols="8"> 
+            <v-text-field
+                v-model="currentAward"
+                class="mb-1"
+                clearable
+                label="Enter Award"
+              ></v-text-field>
+          </v-col>
+          <v-col cols="2" class="py-5">
+            <v-btn
+              @click="addAward"
+              color="#121212"
+              block
+            >
+              Add
+            </v-btn>
+          </v-col>
+          <v-col cols="2" class="py-5">
+            <v-btn
+              @click="this.awardsOverlay = !this.awardsOverlay"
+              color="#121212"
+              block
+            >
+              Done
+            </v-btn>
+          </v-col>
+        </v-row>
+      <br/>
+      </v-card>
+      <v-card class="mx-auto px-10 py-5" width="850px" height="500px">
+        <v-row>
+          <v-list>
+            <v-list-item
+              v-for="award in awards"
+              :key="award"
+              :title= "award"
+              prepend-icon="mdi-arrow-right"
+            >
+              <v-spacer></v-spacer>
+              <template v-slot:append>
+                <v-btn size="small" variant="tonal" @click="removeAward(award)">
+                  <v-icon color="#711429" > mdi-book-open  </v-icon>&nbsp;
+                  Remove
+                </v-btn>
+                &nbsp;
+              </template>
+              <v-divider color="#fff"></v-divider>
+            </v-list-item>
+        </v-list>
+        </v-row>
+      </v-card>
+    </v-overlay>
+
+    <v-overlay
+      :model-value="coursesOverlay"
+      class="align-center justify-center"
+      persistent
+    >
+      <v-card class="mx-auto px-10 py-3" width="850px" height="80px" color="#711429">
+        <v-row >
+          <v-col cols="8"> 
+            <v-text-field
+                v-model="currentCourse"
+                class="mb-1"
+                clearable
+                label="Enter Course"
+              ></v-text-field>
+          </v-col>
+          <v-col cols="2" class="py-5">
+            <v-btn
+              @click="addCourse"
+              color="#121212"
+              block
+            >
+              Add
+            </v-btn>
+          </v-col>
+          <v-col cols="2" class="py-5">
+            <v-btn
+              @click="this.coursesOverlay = !this.coursesOverlay"
+              color="#121212"
+              block
+            >
+              Done
+            </v-btn>
+          </v-col>
+        </v-row>
+      <br/>
+      </v-card>
+      <v-card class="mx-auto px-10 py-5" width="850px" height="500px">
+        <v-row>
+          <v-list>
+            <v-list-item
+              v-for="course in courses"
+              :key="course"
+              :title= "course"
+              prepend-icon="mdi-arrow-right"
+            >
+              <v-spacer></v-spacer>
+              <template v-slot:append>
+                <v-btn size="small" variant="tonal" @click="removeCourse(course)">
+                  <v-icon color="#711429" > mdi-book-open  </v-icon>&nbsp;
+                  Remove
+                </v-btn>
+                &nbsp;
+              </template>
+              <v-divider color="#fff"></v-divider>
+            </v-list-item>
+        </v-list>
+        </v-row>
+      </v-card>
+    </v-overlay>
+
+    <v-overlay
+      :model-value="notesOverlay"
+      class="align-center justify-center"
+      persistent
+    >
+      <v-card class="mx-auto px-10 py-3" width="850px" height="80px" color="#711429">
+        <v-row >
+          <v-col cols="8"> 
+            <v-text-field
+                v-model="currentNote"
+                class="mb-1"
+                clearable
+                label="Enter Note"
+              ></v-text-field>
+          </v-col>
+          <v-col cols="2" class="py-5">
+            <v-btn
+              @click="addNote"
+              color="#121212"
+              block
+            >
+              Add
+            </v-btn>
+          </v-col>
+          <v-col cols="2" class="py-5">
+            <v-btn
+              @click="this.notesOverlay = !this.notesOverlay"
+              color="#121212"
+              block
+            >
+              Done
+            </v-btn>
+          </v-col>
+        </v-row>
+      <br/>
+      </v-card>
+      <v-card class="mx-auto px-10 py-5" width="850px" height="500px">
+        <v-row>
+          <v-list>
+            <v-list-item
+              v-for="note in experienceNote"
+              :key="note"
+              :title= "note"
+              prepend-icon="mdi-arrow-right"
+            >
+              <v-spacer></v-spacer>
+              <template v-slot:append>
+                <v-btn size="small" variant="tonal" @click="removeNote(note)">
+                  <v-icon color="#711429" > mdi-book-open  </v-icon>&nbsp;
+                  Remove
+                </v-btn>
+                &nbsp;
+              </template>
+              <v-divider color="#fff"></v-divider>
+            </v-list-item>
+          </v-list>
+        </v-row>
+      </v-card>
+    </v-overlay>
+
     <v-overlay
       :model-value="loadingOverlay"
       class="align-center justify-center"
@@ -516,9 +759,8 @@
   </template>
   
   <script>
-  import { CohereClient } from "cohere-ai"
-
-    export default {
+  import html2pdf from "html2pdf.js";
+  export default {
       data: () => ({
         step: null,
         currentStep: null,
@@ -528,66 +770,67 @@
         timeout: 2000,
         snackbar: false,
         snackbarMSG: "",
-        jobItmes: ['Job and Personal details', 'Education', 'Experience', 'Skills', 'Preview'],
+        jobItmes: ['Job and Personal details', "Professional Summary", 'Education', 'Experience', 'Skills', 'Preview'],
+
         jobdescription: "",
         jobtitle: "",
+
         fullName: "",
         email: "",
         phoneNumber: "",
-        linkedinURL: "",
         websiteURL: "",
+        location: "",
+
+        professionalSummary:"",
+        
         degree: "",
         major: "",
         institution: "",
-        location: "",
+        eduLocation: "",
         eduStartDate: "",
         eduEndDate: "",
-        websiteURL: "",
+        gpa: "",
+        awards: [],
+        courses: [],
+        educationDetails: [],
+        selectedEducationDetails: [],
+        educationGenText: "",
+
         employer: "",
         jobTitle: "",
         jobLocation: "",
-        jobAccomplishments: "",
         jobStartDate: "",
         jobEndDate: "",
+        experienceNote: [],
+        experienceDetails: [],
+        selectedExperienceDetails: [],
+
         skillName: "",
         skillLevel: "",
         skillGenerated: "",
-        tempalteItems: ['Template 1', 'Template 2', 'Template 3', 'Template 4'],
+        selectedSkills:[],
+        skills: [],
+
+        tempalteItems: ['Template 1'],
         tempalte: "",
+        opt: {
+            margin: 0,
+            filename: 'myResume.pdf',
+            image: { type: 'jpeg', quality: 0.98 },
+            html2canvas: { scale: 2 },
+            jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+        },
+
+        awardsOverlay: false,
+        currentAward: "",
+        coursesOverlay: false,
+        currentCourse: "",
+        notesOverlay: false,
+        currentNote: "",
+        resumeDetails: null,
+        showTemplate: false
       }),
   
-      methods: {
-        onLoad(){
-
-        },
-        clearFields(){
-             
-        },
-        setLoadingOverLay(isShow, message){
-            if(isShow){
-                this.loadingOverlay = true
-                this.loadingMSG = message
-            }
-            else{
-                this.loadingOverlay = false
-                this.loadingMSG = null
-            }
-        },
-        showSnackBar(msg){
-            this.snackbar = true
-            this.snackbarMSG = msg
-        }
-      },
-      watch: {
-      
-      },
-      beforeMount(){
-        this.onLoad()
-      }
-    }
-  </script>
-
-
       methods: {
         onLoad(){
           this.resumeDetails = {
@@ -695,7 +938,8 @@
           this.awards = []
           this.courses = []
         },
-addExperience(){
+        
+        addExperience(){
           var experienceDetail = {
             employer: this.employer,
             jobTitle: this.jobTitle,
@@ -743,6 +987,7 @@ addExperience(){
           this.jobEndDate = ""
           this.experienceNote = []
         },
+
         addSkill(){
           var skill = {
             skillName: this.skillName,
@@ -765,11 +1010,13 @@ addExperience(){
           this.skillLevel = ""
         },
 
-
-
         
-  
-
-
-
-
+        
+      watch: {
+      
+      },
+      beforeMount(){
+        this.onLoad()
+      }
+    }
+  </script>
