5c4bfde6686a0fcae004c2df59c1a6e97550b022
MT549 <monica.turaka@eagles.oc.edu>
2024-07-14 20:30:53 -0500

    Adding services for resume management

--- src/services/UserAuthenticationService.js
+++ src/services/UserAuthenticationService.js
@@ -7,6 +7,17 @@ export default{
     login (credentials) {
         return Api().post('/login', credentials)
     },
-    
+    getAlUsers () {
+        return Api().get('/users')
+    },
+    deleteUser (userID){
+        return Api().delete('/user/' + userID)
+    },
+    updateUser (user){
+        return Api().patch('/user', user)
+    },
+    getUser (id){
+        return Api().get('/user/' + id)
+    }
 }
 
